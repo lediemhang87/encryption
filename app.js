4f3d1c7586e25dbf633a18d1086c13127bb63605
unknown <>
2021-01-24 18:06:55 -0800

    Second commit, level 4

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
 const app = express();
 
  app.use(express.static("public"));
  app.set('view engine', 'ejs');
  app.use(bodyParser.urlencoded({extended: true}));
-
-
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -20,7 +18,7 @@ const usersSchema = new mongoose.Schema( {
   password: String
 })
 
-usersSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+// usersSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 const User = new mongoose.model("User", usersSchema);
 
  app.get("/", function(req,res){
@@ -38,7 +36,7 @@ const User = new mongoose.model("User", usersSchema);
      }
      else{
        if (foundUser){
-         if (req.body.password === foundUser.password){
+         if (md5(req.body.password) === foundUser.password){
            res.render("secrets");
          }
          else{
@@ -59,7 +57,7 @@ const User = new mongoose.model("User", usersSchema);
  app.post("/register", function(req, res){
    const newUser = new User({
      userName: req.body.username,
-     password: req.body.password
+     password: md5(req.body.password)
    })
 
    User.findOne({userName: req.body.username}, function(err, foundUser){
